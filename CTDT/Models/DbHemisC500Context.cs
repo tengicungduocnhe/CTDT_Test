e53c343fb1a36e2909f2b498cb782b5bf3f22335
Thanh Phong Cao <hong99152@gmail.com>
2024-12-08 15:50:35 +0700

    .

--- CTDT/Models/DbHemisC500Context.cs
+++ CTDT/Models/DbHemisC500Context.cs
@@ -627,7 +627,8 @@ public partial class DbHemisC500Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=14.0.22.12;Database=dbHemisC500;User Id=sa;Password=@Abc123456;Encrypt=false;Integrated Security=False;");
+        // => optionsBuilder.UseSqlServer("Server=14.0.22.12;Database=dbHemisC500;User Id=sa;Password=@Abc123456;Encrypt=false;Integrated Security=False;");
+        => optionsBuilder.UseSqlServer("Data Source=125.212.224.205;Initial Catalog=bdxyz_suco;Persist Security Info=True;User ID=bdxyz_1;Password=123123Anh05#;Encrypt=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

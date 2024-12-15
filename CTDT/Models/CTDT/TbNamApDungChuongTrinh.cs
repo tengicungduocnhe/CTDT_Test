79694771aca4ad1a36c668737bd96e480010c07b
Nguyen Duy Tien <nguyenduytien99thaibinh@gmail.com>
2024-12-15 22:03:09 +0700

    ...

--- CTDT/Models/CTDT/TbNamApDungChuongTrinh.cs
+++ CTDT/Models/CTDT/TbNamApDungChuongTrinh.cs
@@ -9,28 +9,29 @@ public partial class TbNamApDungChuongTrinh
     [Display(Name = "ID Năm Áp Dụng Chương Trình")]
     public int IdNamApDungChuongTrinh { get; set; }
 
-    [Display(Name = "Chương trình đào tạo")]
+    [Display(Name = "Chương Trình Đào Tạo")]
     public int? IdChuongTrinhDaoTao { get; set; }
     public string? TenChuongTrinh { get; set; }
 
-    [Display(Name = "Số tín chỉ tối thiểu để tốt nghiệp")]
+    [Display(Name = "Số Tín Chỉ Tối Thiểu Để Tốt Nghiệp")]
     [RegularExpression(@"^[0-9]*$", ErrorMessage = " Chỉ được chứa ký tự số.")]
     public int? SoTinChiToiThieuDeTotNghiep { get; set; }
-    [Display(Name = "Tổng học phí toàn khoá")]
+    [Display(Name = "Tổng Học Phí Toàn Khóa")]
     [RegularExpression(@"^[0-9]*$", ErrorMessage = " Chỉ được chứa ký tự số.")]
     public int? TongHocPhiToanKhoa { get; set; }
 
-    [Display(Name = "Năm áp dụng")]
+    [Display(Name = "Năm Áp Dụng")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
     public DateOnly? NamApDung { get; set; }
 
 
-    [Display(Name = "Chỉ tiêu tuyển sinh hằng năm")]
+    [Display(Name = "Chỉ Tiêu Tuyển Sinh Hàng Năm")]
     [RegularExpression(@"^[0-9]*$", ErrorMessage = " Chỉ được chứa ký tự số.")]
     public int? ChiTieuTuyenSinhHangNam { get; set; }
 
 
+    [Display(Name = "ID Chương Trình Đào Tạo")]
     public virtual TbChuongTrinhDaoTao? IdChuongTrinhDaoTaoNavigation { get; set; }
 }
 
--- CTDT/Models/CTDT/TbQuyetDinhCapPhepChuongTrinhDungChoChuongTrinhNuocNgoai.cs
+++ CTDT/Models/CTDT/TbQuyetDinhCapPhepChuongTrinhDungChoChuongTrinhNuocNgoai.cs
@@ -7,22 +7,22 @@ namespace CTDT.Models;
 public partial class TbQuyetDinhCapPhepChuongTrinhDungChoChuongTrinhNuocNgoai
 
 {
-    [Display(Name = "Id Quyết định cấp phép chương trình dùng cho chương trình nước ngoài")]
+    [Display(Name = "Id Quyết Định Cấp Phép Chương Trình Dùng Cho Chương Trình Nước Ngoài")]
     public int IdQuyetDinhCapPhepChuongTrinhDungChoChuongTrinhNuocNgoai { get; set; }
 
 
-    [Display(Name = "ID chương trình đào tạo")]
+    [Display(Name = "ID Chương Trình Đào Tạo")]
     public int? IdChuongTrinhDaoTao { get; set; }
 
 
-    [Display(Name = "ID loại quyết định")]
+    [Display(Name = "ID loại Quyết Định")]
     public int? IdLoaiQuyetDinh { get; set; }
 
-    [Display(Name = "số quyết định")]
+    [Display(Name = "Số Quyết Định")]
     [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Chỉ được chứa ký tự chữ và số.")]
     public string? SoQuyetDinh { get; set; }
 
-    [Display(Name = "Ngày ban hành quyết định")]
+    [Display(Name = "Ngày Ban Hành Quyết Định")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
     public DateOnly? NgayBanHanhQuyetDinh { get; set; }
@@ -30,9 +30,12 @@ public partial class TbQuyetDinhCapPhepChuongTrinhDungChoChuongTrinhNuocNgoai
     [Display(Name = "ID Hình thức đào tạo")]
     public int? IdHinhThucDaoTao { get; set; }
 
+    [Display(Name = "ID Chương Trình Đào Tạo")]
     public virtual TbChuongTrinhDaoTao? IdChuongTrinhDaoTaoNavigation { get; set; }
 
+    [Display(Name = "ID Hình Thức Đào Tạo")]
     public virtual DmHinhThucDaoTao? IdHinhThucDaoTaoNavigation { get; set; }
 
+    [Display(Name = "ID Loại Quyết Định")]
     public virtual DmLoaiQuyetDinh? IdLoaiQuyetDinhNavigation { get; set; }
 }
